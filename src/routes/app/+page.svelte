a6c1ddbfe6198133e7041e4c02fd09e64bba00c2
Sambhav Choudhary <sambhavc225@outlook.com>
2024-07-11 18:04:02 +0530

    fixed /app

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -4,17 +4,9 @@
 </script>
 
 <div class="flex min-h-screen w-full flex-col">
-	<Navbar active="app" />
-	<main class="my-5">
-		<h1 class="text-center text-9xl font-bold">Aquin.</h1>
-		<h2 class="my-6 text-center text-5xl">
-			Something <span class="font-bold">Cool</span> +
-			<span class="italic">Insane </span>
-		</h2>
-		<h3 class="text-center text-3xl font-medium">
-			Get <span class="italic">answers</span> to the <span class="font-bold">questions</span> like never
-			before!
-		</h3>
+	<Navbar active="aquinpi" />
+	<main class="flex h-[75vh] w-full items-center justify-center">
+		<h1 class="text-9xl font-bold">Cooking...</h1>
 	</main>
 	<Footer active="app" />
 </div>
